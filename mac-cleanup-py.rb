8ce74fe03297fb4b48f8c1743fa14e6770435464
drugsosos <44712637+Drugsosos@users.noreply.github.com>
2022-07-04 02:08:48 +0300

    updated hash for new version

--- mac-cleanup-py.rb
+++ mac-cleanup-py.rb
@@ -2,7 +2,7 @@ class MacCleanupPy < Formula
   desc "Python cleanup script for macOS"
   homepage "https://github.com/Drugsosos/mac-cleanup-py"
   url "https://github.com/Drugsosos/mac-cleanup-py/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "a72248df49dc7e2f590ac8f7696e292869da68d7f4e193480aff6010cef446b5"
+  sha256 "11a43db022a04ef73f19ce9f43cf07b38210a29344185b4c566b7e39cb0e167a"
   license "Apache-2.0"
 
   depends_on "python3"
