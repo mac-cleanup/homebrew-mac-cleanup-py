2ecd1c8c0a880ad6ff9c1dc0bcd78f2179d0c1a1
drugsosos <44712637+Drugsosos@users.noreply.github.com>
2022-07-06 17:04:11 +0300

    updated to v1.1.0

--- mac-cleanup-py.rb
+++ mac-cleanup-py.rb
@@ -1,8 +1,8 @@
 class MacCleanupPy < Formula
   desc "Python cleanup script for macOS"
   homepage "https://github.com/mac-cleanup/mac-cleanup-py"
-  url "https://github.com/mac-cleanup/mac-cleanup-py/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "18d84f8fb178fd5f74b08059a25978ba5e40c09934bc6ed03359075b835887fc"
+  url "https://github.com/mac-cleanup/mac-cleanup-py/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "12372f543e9940849741c34f3b9bb3c9c2eb253ae57894a60e29656f48d35c4c"
   license "Apache-2.0"
 
   depends_on "python3"
