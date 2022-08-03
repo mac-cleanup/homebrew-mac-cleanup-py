4316e40398c22096c2e581996b246794a558e2d9
drugsosos <44712637+Drugsosos@users.noreply.github.com>
2022-08-03 03:56:12 +0300

    Revert "updated to v2.0.0"
    This reverts commit dad5d8b5205741eb13b88c78426184bcf91aa77e.

--- mac-cleanup-py.rb
+++ mac-cleanup-py.rb
@@ -1,20 +1,13 @@
 class MacCleanupPy < Formula
-  include Language::Python::Virtualenv
-
   desc "Python cleanup script for macOS"
   homepage "https://github.com/mac-cleanup/mac-cleanup-py"
-  url "https://github.com/mac-cleanup/mac-cleanup-py/archive/refs/tags/v2.0.0.tar.gz"
-  sha256 "4fefa23c252e36399e858ff0d5f0933e1c66c27acdd6535c85ba54cf49d093fa"
+  url "https://github.com/mac-cleanup/mac-cleanup-py/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "38f6b3d8c69adcfd2644fe8306e48dfc39e1df84b285175e98d01241399d51f2"
   license "Apache-2.0"
 
   depends_on "python3"
 
-  resource "rich" do
-    url "https://pypi.python.org/packages/source/s/rich/rich-12.5.1.tar.gz"
-    sha256 "63a5c5ce3673d3d5fbbf23cd87e11ab84b6b451436f1b7f19ec54b6bc36ed7ca"
-  end
-
   def install
-  	virtualenv_install_with_resources
+  	bin.install 'mac-cleanup'
   end
 end
